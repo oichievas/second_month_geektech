8175b57ae39f9c09136a92d7bc2853aa7f1b9731
Saule <sauleojcieva225@gmail.com>
2022-07-20 15:49:51 +0600

    added homework for lesson 2

---
+++ lesson 1 - homework/app.js
@@ -0,0 +1,69 @@
+// const name = "Arsen"; //string
+
+// let num = 5; //number
+
+// let bool = true; //boolean
+
+// let bool2 = false;
+
+// const obj = { //object
+//   key: "value",
+//   username: "geektech_osh",
+//   followers: 2045,
+//   data: {
+//     first_name: "Nik",
+//     last_name: "Gessl"
+//   }
+// }
+
+// console.log(obj.data.last_name);
+
+// const array = ["da", 4, true, false, "Saule"];
+
+// console.log(array.length)
+// console.log(typeof array)
+
+// let user = prompt("Как вас зовут?");
+
+// console.log("Hello " + user);
+
+// let age = alert("Тебе нет 18!")
+
+// если (условие) {
+//   выражение
+// } иначе {
+//   выражение
+// }
+
+// let user = prompt("Сколько вам лет?");
+
+// if(user<18) {
+//   console.log("Тебе нельзя входить!")
+// } else if (user>18) {
+//   console.log("Тебе можно входить")
+// } else {
+//   console.log("Тебе 18 чувакю Входи!")
+// }
+
+// 2 способ
+//  || - или
+// if(user < 18 || user == 18) {
+//   console.log("Тебе нельзя входить!")
+// } else {
+//   console.log("Тебе 18 чувак. Входи!")
+// }
+
+const object = {
+  user: 'google',
+  instagram: {
+    username: 'Murka',
+    folow: {
+      following: 255,
+      followers: ["Nola", "Mike", 270]
+    }
+  }
+}
+
+console.log(object.instagram.folow.followers[0])
+
+
